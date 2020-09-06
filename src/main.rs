2d0f3c8663238799fe295dd82f7e42a5d1967ee7
paulkernfeld <me@paulkernfeld.com>
2020-09-06 11:43:30 -0400

    Use async/await

--- src/main.rs
+++ src/main.rs
@@ -58,71 +58,62 @@ impl Future for Txis {
     }
 }
 
-struct Compass(&'static i2c1::RegisterBlock);
+async fn get_compass(i2c1: &'static i2c1::RegisterBlock) -> (i16, i16, i16) {
+    i2c1.cr2.write(|w| {
+        w.start().set_bit();
+        w.sadd1().bits(MAGNETOMETER);
+        w.rd_wrn().clear_bit();
+        w.nbytes().bits(1);
+        w.autoend().clear_bit()
+    });
 
-impl Stream for Compass {
-    type Item = (i16, i16, i16);
+    // Wait until we can send more data
+    Txis(&i2c1).await;
 
-    fn poll_next(self: Pin<&mut Self>, cx: &mut Context<'_>) -> Poll<Option<Self::Item>> {
-        let i2c1 = &self.0;
-        i2c1.cr2.write(|w| {
-            w.start().set_bit();
-            w.sadd1().bits(MAGNETOMETER);
-            w.rd_wrn().clear_bit();
-            w.nbytes().bits(1);
-            w.autoend().clear_bit()
-        });
+    // Send the address of the register that we want to read: OUT_X_H_M
+    i2c1.txdr.write(|w| w.txdata().bits(OUT_X_H_M));
 
-        // Wait until we can send more data
-        // TODO await
-        block_on(Txis(i2c1));
+    // Wait until the previous byte has been transmitted
+    while i2c1.isr.read().tc().bit_is_clear() {}
 
-        // Send the address of the register that we want to read: OUT_X_H_M
-        i2c1.txdr.write(|w| w.txdata().bits(OUT_X_H_M));
+    // Broadcast RESTART
+    // Broadcast the MAGNETOMETER address with the R/W bit set to Read
+    i2c1.cr2.modify(|_, w| {
+        w.start().set_bit();
+        w.nbytes().bits(6);
+        w.rd_wrn().set_bit();
+        w.autoend().set_bit()
+    });
 
-        // Wait until the previous byte has been transmitted
-        while i2c1.isr.read().tc().bit_is_clear() {}
+    let mut buffer = [0u8; 6];
+    for byte in &mut buffer {
+        // Wait until we have received something
+        while i2c1.isr.read().rxne().bit_is_clear() {}
 
-        // Broadcast RESTART
-        // Broadcast the MAGNETOMETER address with the R/W bit set to Read
-        i2c1.cr2.modify(|_, w| {
-            w.start().set_bit();
-            w.nbytes().bits(6);
-            w.rd_wrn().set_bit();
-            w.autoend().set_bit()
-        });
-
-        let mut buffer = [0u8; 6];
-        for byte in &mut buffer {
-            // Wait until we have received something
-            while i2c1.isr.read().rxne().bit_is_clear() {}
-
-            *byte = i2c1.rxdr.read().rxdata().bits();
-        }
-        // Broadcast STOP (automatic because of `AUTOEND = 1`)
-
-        let x_h = u16::from(buffer[0]);
-        let x_l = u16::from(buffer[1]);
-        let z_h = u16::from(buffer[2]);
-        let z_l = u16::from(buffer[3]);
-        let y_h = u16::from(buffer[4]);
-        let y_l = u16::from(buffer[5]);
-
-        let x = ((x_h << 8) + x_l) as i16;
-        let y = ((y_h << 8) + y_l) as i16;
-        let z = ((z_h << 8) + z_l) as i16;
-
-        Poll::Ready(Some((x, y, z)))
+        *byte = i2c1.rxdr.read().rxdata().bits();
     }
+    // Broadcast STOP (automatic because of `AUTOEND = 1`)
+
+    let x_h = u16::from(buffer[0]);
+    let x_l = u16::from(buffer[1]);
+    let z_h = u16::from(buffer[2]);
+    let z_l = u16::from(buffer[3]);
+    let y_h = u16::from(buffer[4]);
+    let y_l = u16::from(buffer[5]);
+
+    let x = ((x_h << 8) + x_l) as i16;
+    let y = ((y_h << 8) + y_l) as i16;
+    let z = ((z_h << 8) + z_l) as i16;
+
+    (x, y, z)
 }
 
 #[entry]
 fn main() -> ! {
     let (i2c1, mut delay, mut itm) = aux14::init();
-    let mut compass = Compass(i2c1);
 
     loop {
-        iprintln!(&mut itm.stim[0], "{:?}", block_on(compass.next()));
+        iprintln!(&mut itm.stim[0], "{:?}", block_on(get_compass(i2c1)));
 
         delay.delay_ms(1_000_u16);
     }
