2d9a6c9773ac3c97e76bf60948ccb97df9ca6e35
paulkernfeld <me@paulkernfeld.com>
2020-09-06 10:47:55 -0400

    Run a future

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,10 @@
-#![deny(unsafe_code)]
 #![no_main]
 #![no_std]
 
 #[allow(unused_imports)]
 use aux14::{entry, iprint, iprintln, prelude::*};
+use core::future::Future;
+use core::task::{Context, Poll, RawWaker, RawWakerVTable, Waker};
 
 // Slave address
 const MAGNETOMETER: u8 = 0b001_1110;
@@ -11,6 +12,34 @@ const MAGNETOMETER: u8 = 0b001_1110;
 // Addresses of the magnetometer's registers
 const OUT_X_H_M: u8 = 0x03;
 const IRA_REG_M: u8 = 0x0A;
+
+// https://users.rust-lang.org/t/simplest-possible-block-on/48364/2?u=occupy_paul_st
+unsafe fn rwclone(_p: *const ()) -> RawWaker {
+    make_raw_waker()
+}
+unsafe fn rwwake(_p: *const ()) {}
+unsafe fn rwwakebyref(_p: *const ()) {}
+unsafe fn rwdrop(_p: *const ()) {}
+
+static VTABLE: RawWakerVTable = RawWakerVTable::new(rwclone, rwwake, rwwakebyref, rwdrop);
+
+fn make_raw_waker() -> RawWaker {
+    static DATA: () = ();
+    RawWaker::new(&DATA, &VTABLE)
+}
+
+fn block_on<F: Future>(future: F) -> F::Output {
+    pin_utils::pin_mut!(future);
+    let waker = &unsafe { Waker::from_raw(make_raw_waker()) };
+    let mut cx = Context::from_waker(waker);
+    loop {
+        if let Poll::Ready(output) = future.as_mut().poll(&mut cx) {
+            return output;
+        }
+    }
+}
+
+async fn immediate() {}
 
 #[entry]
 fn main() -> ! {
@@ -69,6 +98,7 @@ fn main() -> ! {
 
         iprintln!(&mut itm.stim[0], "{:?}", (x, y, z));
 
+        block_on(immediate());
 
         delay.delay_ms(1_000_u16);
     }
